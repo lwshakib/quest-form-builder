4609c9c4a38726cb194d7a67967ff1930b0757b2
Shakib Khan <leadwithshakib@gmail.com>
2026-01-20 01:02:41 +0600

    feat: add main dashboard layout wrapper

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic";
+
+import ClientLayout from "./client-layout";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ClientLayout>{children}</ClientLayout>;
+}
